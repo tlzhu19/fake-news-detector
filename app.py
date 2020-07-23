3d2b32494219f84b565c19119058d9371cce249a
Tiffany Zhu <tiffanyzhu19@gmail.com>
2020-07-23 14:05:05 -0400

    add model

--- app.py
+++ app.py
@@ -15,26 +15,47 @@ def read_article_url(url):
 
 st.title('Fake News Detector')
 
-loaded_text_clf = load('models/nb_model.joblib') 
+# Load models
+nb_model = load('models/MultinomialNB_model.joblib') 
+lr_model = load('models/LogisticRegression_model.joblib') 
 
-st.subheader('Input text or give a url link to an article:')
+model_names = {
+    'Multinomial Naive Bayes': nb_model, 
+    'Logistic Regression': lr_model
+}
+
+# 1. Select Model
+st.subheader('1. Select Model')
+# list of possible models to run
+model_to_run = st.selectbox(
+    'Model to run: ',
+    list(model_names.keys())
+    )
+
+loaded_text_clf = model_names[model_to_run]
+
+# 2. Input article
+st.subheader('2. Input text or give a url link to an article:')
 article_url = st.text_input('Article URL: ')
 article_text = st.text_area('Article Text: ', height=200)
 
+# convert article url to text
 if article_url:
     article_text = read_article_url(article_url)
 
+# choice to display imported text
 display_input = st.checkbox('Display Input Text')
 if display_input:
     st.write('Input text', article_text)
 
-
+# run prediction
 with st.spinner('Predicting...'):
     predicted = loaded_text_clf.predict([article_text])[0]
     probabilities = list(loaded_text_clf.predict_proba([article_text])[0])
     predicted_probability = probabilities[predicted]
     predicted_text = 'REAL' if predicted else 'FAKE'
 
+# return result
 if article_text:
     st.subheader('This article is {} with probability {:.2%}'.format(predicted_text, predicted_probability))
     plt.pie(probabilities, 
