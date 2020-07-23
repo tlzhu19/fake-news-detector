a290ff13384607888228b83fe4b56207a2f7e141
Tiffany Zhu <tiffanyzhu19@gmail.com>
2020-07-23 13:37:48 -0400

    Add pie chart

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st 
 from joblib import load
 import newspaper
+import time 
+import matplotlib.pyplot as plt
 
 
 def read_article_url(url):
@@ -17,7 +19,7 @@ loaded_text_clf = load('models/nb_model.joblib')
 
 st.subheader('Input text or give a url link to an article:')
 article_url = st.text_input('Article URL: ')
-article_text = st.text_input('Article Text: ')
+article_text = st.text_area('Article Text: ', height=200)
 
 if article_url:
     article_text = read_article_url(article_url)
@@ -27,10 +29,20 @@ if display_input:
     st.write('Input text', article_text)
 
 
-predicted = loaded_text_clf.predict([article_text])
-predicted_prob = loaded_text_clf.predict_proba([article_text])[0][predicted][0]
-predicted_text = 'REAL' if predicted else 'FAKE'
-st.subheader('This article is {} with probability {:.2%}'.format(predicted_text, predicted_prob))
+with st.spinner('Predicting...'):
+    predicted = loaded_text_clf.predict([article_text])[0]
+    probabilities = list(loaded_text_clf.predict_proba([article_text])[0])
+    predicted_probability = probabilities[predicted]
+    predicted_text = 'REAL' if predicted else 'FAKE'
+
+if article_text:
+    st.subheader('This article is {} with probability {:.2%}'.format(predicted_text, predicted_probability))
+    plt.pie(probabilities, 
+        labels=['Fake', 'Real'], 
+        colors=['#e0a4a4', '#c0ffb5'], 
+        autopct='%1.1f%%',
+       )
+    st.pyplot()
 
 
 
